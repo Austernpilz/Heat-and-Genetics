5fa6d9d08de39e7c71616d1daa9b23344cb07dff
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2026-07-15 10:38:21 +0200

    wip

--- src/ensembl/fetch_ensembl.py
+++ src/ensembl/fetch_ensembl.py
@@ -148,7 +148,7 @@ def translate_to_rsid(path_to_data, hgvs):
         print(f"{datetime.now().strftime('%H%M')} VEP got {hgvs} to rsid")
         sleep(0.1)
         rsids = []
-        print(found)
+        print(decoded)
         for found in decoded:
             for base, possible_ids in found.items():
                 rsids += possible_ids.get("id", [])
