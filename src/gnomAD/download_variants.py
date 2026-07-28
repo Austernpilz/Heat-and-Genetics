3577fb95a825438b6caeee989c469b7b1982f44c
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2026-07-28 21:07:47 +0200

    wip

--- src/gnomAD/download_variants.py
+++ src/gnomAD/download_variants.py
@@ -250,7 +250,7 @@ def fetch_from_ensembl_id_as_json(query, ensembl_id, data_path, name, download=F
                             timeout=180
                             )
             data = response.json().get("data", {}).get("gene", {})
-
+            sleep(6)
             if data:
                 with open(file_name, 'w') as file:
                     json.dump(data, file)
@@ -267,14 +267,9 @@ def fetch_from_ensembl_id_as_json(query, ensembl_id, data_path, name, download=F
 
 def try_fetching_(query, id, data_path, name, files=[]):
     counter_for_fail = 0
-    time_start = datetime.now().total_seconds() + 1
     time_elapsed = 6
     while counter_for_fail < 4:
-        if time_elapsed < 7:
-            sleep(7 - time_elapsed)
-        time_elapsed = time_start - datetime.now().total_seconds()
         file = fetch_from_ensembl_id_as_json(query, id, data_path, name)
-        time_start = datetime.now().total_seconds() + 1
         if file is None:
             counter_for_fail += 1 #download failed
         elif file:
